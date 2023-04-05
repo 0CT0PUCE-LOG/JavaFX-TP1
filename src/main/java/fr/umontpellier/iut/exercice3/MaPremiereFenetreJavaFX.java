6777318ef61c5a1df8d93f218e01e5feac5606e6
starcks <soren.starck@etu.umontpellier.fr>
2023-04-05 16:58:30 +0200

    ex 3

--- src/main/java/fr/umontpellier/iut/exercice3/MaPremiereFenetreJavaFX.java
+++ src/main/java/fr/umontpellier/iut/exercice3/MaPremiereFenetreJavaFX.java
@@ -1,9 +1,14 @@
 package fr.umontpellier.iut.exercice3;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -22,13 +27,31 @@ public class MaPremiereFenetreJavaFX extends Application {
         primaryStage.setWidth(400);
         primaryStage.setHeight(400);
 
+
         VBox vBox = new VBox();
         vBox.setAlignment(javafx.geometry.Pos.CENTER);
-        Label bjr = new Label("Bonjour à tous !");
+        Label bonjour = new Label("Bonjour à tous !");
         TextField saisir_nom = new TextField();
         saisir_nom.setMaxWidth(150);
-        Button btn = new Button("Dire bonjour");
-        vBox.getChildren().addAll(bjr, saisir_nom, btn);
+        Button bouton = new Button();
+        ImageView imageButton= new ImageView("https://gitlabinfo.iutmontp.univ-montp2.fr/ihm/tp1/-/raw/master/images/logo.jpeg");
+        bouton.setGraphic(imageButton);
+        final EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+            public void handle(ActionEvent actionEvent) {
+                if(saisir_nom.getText().equals("César")){
+                    bonjour.setText("Bonjour à toi César");
+                }
+            }
+        };
+        //bouton.addEventHandler(MouseEvent.MOUSE_CLICKED, event);
+        bouton.setOnAction(event);
+        vBox.getChildren().addAll(bonjour, saisir_nom, bouton);
+        saisir_nom.setOnKeyPressed( ActionEvent -> {
+            if( ActionEvent.getCode() == KeyCode.ENTER ) {
+                //call event action
+            }
+        } );
+
 
         Pane pane = new Pane();
         pane.getChildren().add(vBox);
