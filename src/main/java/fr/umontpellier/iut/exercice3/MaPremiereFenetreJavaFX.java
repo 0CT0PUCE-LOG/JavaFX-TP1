968198f14ad612476a90bfdd3c0c731907d7f098
starcks <soren.starck@etu.umontpellier.fr>
2023-04-05 17:13:32 +0200

    ex 3

--- src/main/java/fr/umontpellier/iut/exercice3/MaPremiereFenetreJavaFX.java
+++ src/main/java/fr/umontpellier/iut/exercice3/MaPremiereFenetreJavaFX.java
@@ -2,15 +2,20 @@ package fr.umontpellier.iut.exercice3;
 
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -31,33 +36,35 @@ public class MaPremiereFenetreJavaFX extends Application {
         VBox vBox = new VBox();
         vBox.setAlignment(javafx.geometry.Pos.CENTER);
         Label bonjour = new Label("Bonjour à tous !");
+        bonjour.setFont(Font.font("Courrier", FontWeight.BOLD, 30));
+
         TextField saisir_nom = new TextField();
         saisir_nom.setMaxWidth(150);
+        saisir_nom.setFont(Font.font("Courrier", FontWeight.NORMAL, 15));
+
         Button bouton = new Button();
-        ImageView imageButton= new ImageView("https://gitlabinfo.iutmontp.univ-montp2.fr/ihm/tp1/-/raw/master/images/logo.jpeg");
+        ImageView imageButton= new ImageView("exercice3/Bonjour.jpg");
         bouton.setGraphic(imageButton);
-        final EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent actionEvent) {
-                if(saisir_nom.getText().equals("César")){
-                    bonjour.setText("Bonjour à toi César");
-                }
+        final EventHandler event = new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                bonjour.setText("Bonjour à toi "+saisir_nom.getText()+" !");
             }
         };
-        //bouton.addEventHandler(MouseEvent.MOUSE_CLICKED, event);
         bouton.setOnAction(event);
+        saisir_nom.setOnAction(event);
+
+
         vBox.getChildren().addAll(bonjour, saisir_nom, bouton);
-        saisir_nom.setOnKeyPressed( ActionEvent -> {
-            if( ActionEvent.getCode() == KeyCode.ENTER ) {
-                //call event action
-            }
-        } );
 
 
         Pane pane = new Pane();
         pane.getChildren().add(vBox);
 
+        Scene scene = new Scene(pane);
+        scene.getStylesheets().add(getClass().getClassLoader().getResource("exercice3/Bonjour.css").toExternalForm());
 
-        primaryStage.setScene(new javafx.scene.Scene(pane));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 }
