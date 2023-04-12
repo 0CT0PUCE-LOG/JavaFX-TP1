4753bef4a66836a4238b280a8de5ec46fb678e7b
starcks <soren.starck@etu.umontpellier.fr>
2023-04-12 12:16:39 +0200

    dico

--- src/main/java/fr/umontpellier/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/umontpellier/iut/exercice6/IHMPendu.java
@@ -1,17 +1,41 @@
 package fr.umontpellier.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class IHMPendu extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //setup mot à trouver
+        Dico dico = new Dico();
+        String mot = dico.getMot();
+        System.out.println(mot);
+
+        //setup scene
         primaryStage.setTitle("Jeu du pendu");
         primaryStage.setWidth(500);
         primaryStage.setHeight(550);
 
-        // A completer
+        Label label_nbvie = new Label("Nombre de vie : ");
+
+        Label lebel_mot = new Label("");
+
+        TextField textField = new TextField();
+        textField.setPrefWidth(100);
+        textField.setPrefHeight(10);
+        //textField.setLayoutX(50);
+        //textField.setLayoutY(50);
+
+        VBox vBox = new VBox();
+        vBox.getChildren().addAll(label_nbvie, textField);
+
+        Scene scene = new Scene(vBox);
+        primaryStage.setScene(scene);
 
         primaryStage.show();
     }
