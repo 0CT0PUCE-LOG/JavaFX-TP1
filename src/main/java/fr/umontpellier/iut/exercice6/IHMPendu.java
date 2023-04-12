4a83635a93115ccfa453910379c3b8f43e862e8d
starcks <soren.starck@etu.umontpellier.fr>
2023-04-12 17:56:28 +0200

    pacman

--- src/main/java/fr/umontpellier/iut/exercice6/IHMPendu.java
+++ src/main/java/fr/umontpellier/iut/exercice6/IHMPendu.java
@@ -1,7 +1,10 @@
 package fr.umontpellier.iut.exercice6;
 
 import javafx.application.Application;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
@@ -16,14 +19,17 @@ public class IHMPendu extends Application {
         String mot = dico.getMot();
         System.out.println(mot);
 
+        //setup nombre de vies
+        int nbVie = 10;
+
         //setup scene
         primaryStage.setTitle("Jeu du pendu");
         primaryStage.setWidth(500);
         primaryStage.setHeight(550);
 
-        Label label_nbvie = new Label("Nombre de vie : ");
+        Label label_nbvie = new Label("Nombre de vie : "+ nbVie);
 
-        Label lebel_mot = new Label("");
+        Label label_mot = new Label(cacherMot(mot));
 
         TextField textField = new TextField();
         textField.setPrefWidth(100);
@@ -31,8 +37,22 @@ public class IHMPendu extends Application {
         //textField.setLayoutX(50);
         //textField.setLayoutY(50);
 
+        //submit buuton
+        Button bouton = new Button();
+        final EventHandler event = new EventHandler() {
+            @Override
+            public void handle(Event event) {
+                bonjour.setText("Bonjour à toi "+saisir_nom.getText()+" !");
+            }
+        };
+        bouton.setOnAction(event);
+
         VBox vBox = new VBox();
-        vBox.getChildren().addAll(label_nbvie, textField);
+        vBox.getChildren().addAll(label_nbvie,label_mot, textField);
+        vBox.setAlignment(javafx.geometry.Pos.CENTER);
+        vBox.setSpacing(15);
+
+
 
         Scene scene = new Scene(vBox);
         primaryStage.setScene(scene);
@@ -42,5 +62,13 @@ public class IHMPendu extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public String cacherMot(String mot) {
+        String motCache = "";
+        for (int i = 0; i < mot.length(); i++) {
+            motCache += "*";
+        }
+        return motCache;
     }
 }
